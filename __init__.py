3543df89f0353b10d20a5455302110b0db6c4aef
roxgib <33942237+roxgib@users.noreply.github.com>
2022-01-20 11:01:02 +1100

    Refactor study_ahead() to handle multiple decks

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
 """
 Todo:
-    - Pick best for study ahead all decks
-    - Implement CollectionOp for filtered deck creation
-    - Better handling of child decks
+    - better handling of child decks
+    - gui options
 """
 
 from aqt import mw
@@ -18,34 +17,17 @@ def sort_cards():
     ids = mw.col.find_cards('deck:"Study Ahead" deck:filtered')
     ids = sorted(ids, key=lambda card_id: underdue_ratio(card_id, mw))
     cards = [mw.col.get_card(id) for id in ids]
+    
     for i, card in enumerate(cards):
+
         card.due = int((-10000)+i)
+
     mw.col.update_cards(cards)
 
-def create_filtered_deck(deck_id, tt = True) -> int:
-    config = mw.addonManager.getConfig(__name__)
-    max_days_ahead = config['max_days_ahead']
-    max_cards = config['max_cards']
-    max_multiple = config['max_multiple']
+def create_filtered_deck(card_ids, deck_name):
+    filter_string = '(' + ' OR '.join([f'cid:{id}' for id in card_ids]) + ') -is:due -is:learn -is:suspended -is:buried -rated:1'
 
-    deck_name = mw.col.decks.get(deck_id)['name']
-    
-    undo_entry = mw.col.add_custom_undo_entry(f'Create Study Ahead Deck for "{deck_name}"')
-    
-    if (old_f_deck := mw.col.decks.id_for_name('Study Ahead::' + deck_name)) != None:
-        mw.col.decks.remove([old_f_deck])
-
-    ids = mw.col.find_cards(f'deck:"{deck_name}" prop:due<={max_days_ahead} -is:due -is:learn -is:suspended -is:buried')
-    ids = [id for id in sorted(ids, key = lambda card_id: underdue_ratio(card_id, mw)) if (underdue_ratio(id) > max_multiple)]
-    ids = ids[:max_cards]
-
-    if len(ids) == 0: 
-        if tt: tooltip(f"Couldn't find any suitable cards to study ahead.")
-        return
-
-    filter_string = '(' + ' OR '.join([f'cid:{id}' for id in ids]) + ')' + ' -is:due -is:learn -is:suspended -is:buried -rated:1'
-
-    f_deck_id = mw.col.decks.new_filtered('Study Ahead::' + deck_name)
+    f_deck_id = mw.col.decks.new_filtered(deck_name)
     f_deck = mw.col.decks.current()
 
     f_deck['resched'] = True
@@ -55,29 +37,84 @@ def create_filtered_deck(deck_id, tt = True) -> int:
     mw.col.decks.save(f_deck)
     mw.col.sched.rebuild_filtered_deck(f_deck_id)
 
-    if tt: sort_cards()
+    return f_deck_id
+
+def study_ahead(deck_ids: list) -> int:
+    config = mw.addonManager.getConfig(__name__)
+    max_days_ahead = config['max_days_ahead']
+    max_cards = config['max_cards']
+    min_multiple = config['min_multiple']
+
+    if len(deck_ids) == 1: # Enable tooltip on deck creation
+        tt=True 
+    else: # Limit total cards accross all decks
+        tt = False
+        deck_names = [mw.col.decks.get(deck_id)['name'] for deck_id in deck_ids]
+        deck_string = ' OR '.join([f'deck:"{deck_name}"' for deck_name in deck_names])
+        t_ids = mw.col.find_cards(f'({deck_string}) prop:due<={max_days_ahead} -is:due -is:learn -is:suspended -is:buried')
+        t_ids = [id for id in sorted(t_ids, key = lambda card_id: underdue_ratio(card_id, mw), reverse=True) if (underdue_ratio(id) > min_multiple)]
+        t_ids = t_ids[:max_cards]
+
+        if (total_cards := len(t_ids)) == 0:
+            tooltip(f"Couldn't find any suitable cards to study ahead.")
+            return
+
+        if underdue_ratio(t_ids[-1]) > min_multiple:
+            min_multiple = underdue_ratio(t_ids[-1])
+
+    deck_count = 0
+
+    undo_entry = mw.col.add_custom_undo_entry(f'Create Study Ahead Deck')    
+
+    for deck_id in deck_ids:
+        deck_name = mw.col.decks.get(deck_id)['name']
+        
+        if (old_f_deck := mw.col.decks.id_for_name('Study Ahead::' + deck_name)) != None:
+            mw.col.decks.remove([old_f_deck])
+
+        ids = mw.col.find_cards(f'deck:"{deck_name}" prop:due<={max_days_ahead} -is:due -is:learn -is:suspended -is:buried')
+        ids = [id for id in sorted(ids, key = lambda card_id: underdue_ratio(card_id, mw)) if (underdue_ratio(id) >= min_multiple)]
+        ids = ids[:max_cards]
+
+        if tt: total_cards = len(ids)
+
+        if len(ids) == 0: 
+            if tt: tooltip(f"Couldn't find any suitable cards to study ahead."); return
+            continue
+
+        create_filtered_deck(ids, 'Study Ahead::' + deck_name)
+
+        deck_count += 1
+
+    if tt: tooltip(f"Created filtered deck 'Study Ahead::{deck_name}' with {len(ids)} card{'' if (len(ids) == 1) else 's'}.")
+
+    sort_cards()
+
+    mw.col.merge_undo_entries(undo_entry)
 
     mw.reset()
 
-    if tt: tooltip(f"Created filtered deck 'Study Ahead::{deck_name}' with {len(ids)} card{'' if (len(ids) == 1) else 's'}.")
-    
-    mw.col.merge_undo_entries(undo_entry)
-    
-    return f_deck_id
+    return total_cards
 
 def on_reload_all() -> None:
     undo_entry = mw.col.add_custom_undo_entry(f'Reload All Study Ahead Decks')
     decklist = [mw.col.decks.id_for_name(deck.name[13:]) for deck in mw.col.decks.all_names_and_ids() if deck.name[:13] == 'Study Ahead::']
-    count = len([id for id in [create_filtered_deck(id, tt=False) for id in decklist] if id != None])
-    sort_cards()
-    tooltip(f"Reloaded {count} deck{'' if count == 1 else 's'} for study ahead.")
+    decklist = [id for id in decklist if id != None]
+    mw.col.decks.remove([mw.col.decks.id_for_name('Study Ahead')])
+    count = study_ahead(decklist)
+    if count == None: return
+    tooltip(f"Reloaded decks for study ahead with a total of {count} card{'' if count == 1 else 's'}.")
     mw.col.merge_undo_entries(undo_entry)
 
 def on_study_ahead_all() -> None:
     undo_entry = mw.col.add_custom_undo_entry(f'Study Ahead All Decks')
-    decklist = [deck.id for deck in mw.col.decks.all_names_and_ids() if '::' not in deck.name and not mw.col.decks.is_filtered(deck.id) and deck.name != 'Study Ahead']
-    count = len([id for id in [create_filtered_deck(id, tt=False) for id in decklist] if id != None])
-    sort_cards()
+    try:
+        mw.col.decks.remove([mw.col.decks.id_for_name('Study Ahead')])
+    except:
+        pass
+    deck_list = [deck.id for deck in mw.col.decks.all_names_and_ids() if '::' not in deck.name and not mw.col.decks.is_filtered(deck.id) and deck.name != 'Study Ahead']
+    count = study_ahead([deck for deck in deck_list if deck != None])
+    if count == None: return
     tooltip(f"Created {count} deck{'' if count == 1 else 's'} for study ahead.")
     mw.col.merge_undo_entries(undo_entry)
 
@@ -88,14 +125,14 @@ mw.form.menuTools.addAction(study_ahead_all)
 def on_show_options(menu, deck_id) -> None:
     if (deck := mw.col.decks.get(deck_id))['name'][:11] != 'Study Ahead' and not mw.col.decks.is_filtered(deck_id):
         action = menu.addAction('Study Ahead')
-        action.triggered.connect(lambda _, did=deck_id: create_filtered_deck(did))
+        action.triggered.connect(lambda _, did=deck_id: study_ahead([did]))
     elif deck['name'] ==  'Study Ahead':
         action = menu.addAction('Reload All')
         action.triggered.connect(on_reload_all)
     elif deck['name'][:13] ==  'Study Ahead::':
         home_deck_id = mw.col.decks.id_for_name(deck['name'][13:])
         action = menu.addAction('Reload')
-        action.triggered.connect(lambda _, did=home_deck_id: create_filtered_deck(did))
+        action.triggered.connect(lambda _, did=home_deck_id: study_ahead([did]))
 
 gui_hooks.deck_browser_will_show_options_menu.append(on_show_options)
 
